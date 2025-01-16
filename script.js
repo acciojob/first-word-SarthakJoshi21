458ae87da454f8bfaca17fe394d81c9d9d7b25a8
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2025-01-16 21:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
-  // your code here
+  // Trim any leading whitespace from the string
+  s = s.trim();
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+
+  // If a space is found, return the substring before the space
+  if (spaceIndex !== -1) {
+    return s.substring(0, spaceIndex);
+  }
+
+  // If no space is found, return the entire string (it's a single word)
+  return s;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
